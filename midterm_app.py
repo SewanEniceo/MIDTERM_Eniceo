7dfaf1037a42f46012420ae990b0c00b68273910
NetAcad DEVASC <changeme@example.com>
2023-11-13 04:40:27 +0000

    Question 22

--- midterm_app.py
+++ midterm_app.py
@@ -6,7 +6,7 @@ login = Flask(__name__)
 
 @login.route("/")
 def main():
-    return render_template("index.html")
+    return render_template("login.html")
 
 if __name__ == "__main__":
     login.run(host="0.0.0.0", port=5000)
