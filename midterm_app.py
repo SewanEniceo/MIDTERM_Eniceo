124b33a055071915843a18070c81e9ffd21e9e97
NetAcad DEVASC <changeme@example.com>
2023-11-13 05:19:07 +0000

    Midterm Exam Question 22

--- midterm_app.py
+++ midterm_app.py
@@ -4,9 +4,21 @@ from flask import render_template
 
 login = Flask(__name__)
 
+
 @login.route("/")
 def main():
     return render_template("login.html")
 
+@login.route("/registration.html")
+def registration():
+    return render_template("registration.html")
+
+@login.route("/login.html")
+def backTologin():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     login.run(host="0.0.0.0", port=5000)
+
+
+    
